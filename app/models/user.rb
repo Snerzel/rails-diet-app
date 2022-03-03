bf7a6c4fc0bab5a8103372efde949250080ac737
Snerzel <snerzel@github.com>
2022-03-02 18:34:49 -0800

    added user create model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+
+    def new
+
+    end
+
+    def create
+        @user = User.new
+        @user.name = params[:name]
+        @user.email = params[:email]
+        @user.password_digest = params[:password_digest]
+        @user.age = params[:age]
+        @user.weight = params[:weight]
+        @user.health = params[:health]
+        @user.diet_type = params[:diet_type]
+        @user.restrictions = params[:restrictions]
+        @user.save
+
+        redirect_to '/'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/user-signup', to: 'users#new'
+  post 'users', to: 'users#create'
 end
