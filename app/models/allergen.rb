eaaabfe832237cc04a8661cee7f10da93100825c
noahaklem <83715460+noahaklem@users.noreply.github.com>
2022-04-21 20:04:26 -0600

    fix little things about the meals number 2 on the readme.

--- app/models/allergen.rb
+++ app/models/allergen.rb
@@ -1,7 +1,6 @@
 class Allergen < ApplicationRecord
-    belongs_to :meals
-
-
-
-    
+    belongs_to :meal
+    # relationship error active_support/inflector/methods.rb:274:in `const_get' (key is the active_support)
+    # fix change belongs_to :meals to belongs_to :meal (not plural) because one allergen belongs to one meal
+  
 end
--- db/migrate/20220307233509_create_allergens.rb
+++ db/migrate/20220307233509_create_allergens.rb
@@ -1,4 +1,6 @@
-class CreateAllergen < ActiveRecord::Migration[6.1]
+class CreateAllergens < ActiveRecord::Migration[6.1]
+  # error = uninitialized constant CreateAllergens
+  # fix = change from class CreateAllergen to CreateAllergen(s) plural because this table will hold many allergens
   def change
     create_table :allergens do |t|
       t.string :ingredients
